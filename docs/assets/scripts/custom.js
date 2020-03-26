59e7a3f3e7ed0034d1671f13d568dd528a019fe1
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-03-26 12:40:05 +1100

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
 			alert( "second success" );
 			alert(JSON.stringify(data));
 			$('#wuStatus').text("WU credit check complete.");
+			$('#wuStatusTable').bootstrapTable('removeAll');
 			$('#wuStatusTable').bootstrapTable({data: data});
 			$('#wuStatusData').show();
 		})
