0170eb7ec7085505505dda943cb05c7886047f80
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-03-26 14:36:59 +1100

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -2,15 +2,22 @@ $(document).ready(function () {
 	'use strict';
 	// Calculate QRB
 	$('#calculateQRB').on('click', function (e) {
-		e.preventDefault();
-		var basePoints = $('#projectBasePoints').val();
-		var k = $('#projectBonusFactor').val();
-		var deadlineLength = $('#projectDeadlineLength').val();
-		var wuStartTime = new Date($('#wuStartTime').val());
-		var wuEndTime = new Date($('#wuEndTime').val());
-		var wuDuration = (wuEndTime - wuStartTime)/1000/86400;
-		var bonusFactor = Math.max(1, Math.sqrt(deadlineLength * k / wuDuration)) ;
-		$('#wuTotal').val(basePoints*bonusFactor);
+		if($(this).closest('form')[0].checkValidity()) {
+			e.preventDefault();
+
+			var basePoints = $('#projectBasePoints').val();
+			var k = $('#projectBonusFactor').val();
+			var deadlineLength = $('#projectDeadlineLength').val();
+			var wuStartTime = new Date($('#wuStartTime').val());
+			var wuEndTime = new Date($('#wuEndTime').val());
+
+			var wuDuration = (wuEndTime - wuStartTime)/1000/86400;
+			var bonusFactor = Math.max(1, Math.sqrt(deadlineLength * k / wuDuration)) ;
+
+			$('#wuTotal').val(basePoints*bonusFactor);
+		} else {
+			$(this).closest('form')[0].reportValidity();
+		}
 	});
 
 	// Fetch credit for WU
@@ -18,10 +25,10 @@ $(document).ready(function () {
 		if($(this).closest('form')[0].checkValidity()) {
 			e.preventDefault();
 
-			var projectId = $('#projectId').val()
-			var runId = $('#runId').val()
-			var cloneId = $('#cloneId').val()
-			var genId = $('#genId').val()
+			var projectId = $('#projectId').val();
+			var runId = $('#runId').val();
+			var cloneId = $('#cloneId').val();
+			var genId = $('#genId').val();
 
 			var creditAPIURL = 'https://api.foldingathome.org/project/' + projectId + '/run/' + runId + '/clone/' + cloneId + '/gen/' + genId;
 			
@@ -30,12 +37,14 @@ $(document).ready(function () {
 			var jqxhr = $.getJSON(creditAPIURL)
 			.done(function(data) {
 				$('#wuStatus').text('WU credit check complete for ' + wuDescription + '.');
+				$('#wuStatus').removeClass('bad').addClass('good');
 				$('#wuStatusTable').bootstrapTable('removeAll');
 				$('#wuStatusTable').bootstrapTable({data: data, formatNoMatches: function () {return 'No credits found.';}});
 				$('#wuStatusData').show();
 			})
 			.fail(function(data) {
 				$('#wuStatus').text('An error occured when checking WU credits.');
+				$('#wuStatus').removeClass('good').addClass('bad');
 				$('#wuStatusTable').bootstrapTable('removeAll');
 				$('#wuStatusData').hide();
 			})
@@ -46,6 +55,7 @@ $(document).ready(function () {
 				}, 5000);
 			});
 			$('#wuStatus').text('Checking for WU credits...');
+			$('#wuStatus').removeClass();
 			$('#wuStatusData').hide();
 			$('#wuStatusTable').bootstrapTable('removeAll');
 		} else {
