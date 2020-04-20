f2ce06758ccfa8a08f68e68d0e0256554342f426
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-04-20 22:12:43 +1000

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -146,6 +146,8 @@ function prcgProgress2() {
 		percentage = Math.round((((100 * totalGensCompleted) / totalGensForRun) + Number.EPSILON) * 100) / 100;
 		$('#prcg2ProgressBar').html(getProgressBar(percentage, colorClass[colorClassIndex]));
 		$('#prcg2Table').show();
+		$('#prcg2UpToProject').show();
+		$('prcg2UpToProjectURL').attr('href', './prcgProgress?project=' + projectId);
 
 		if (runId != 0) {
 			$('#prcg2PreviousRun').show();
