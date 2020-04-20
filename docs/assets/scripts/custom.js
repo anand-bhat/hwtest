60c9b14f139a386b75e98aeb0d194f5ef0cf2a96
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-04-20 15:27:09 +1000

    T

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -1,5 +1,106 @@
+var colorClass = {
+	0: '#FF0000',
+	1: '#FF1100',
+	2: '#FF2300',
+	3: '#FF3400',
+	4: '#FF4600',
+	5: '#FF5700',
+	6: '#FF6900',
+	7: '#FF7B00',
+	8: '#FF8C00',
+	9: '#FF9E00',
+	10: '#FFAF00',
+	11: '#FFC100',
+	12: '#FFD300',
+	13: '#FFE400',
+	14: '#FFF600',
+	15: '#F7FF00',
+	16: '#E5FF00',
+	17: '#D4FF00',
+	18: '#C2FF00',
+	19: '#B0FF00',
+	20: '#9FFF00',
+	21: '#8DFF00',
+	22: '#7CFF00',
+	23: '#6AFF00',
+	24: '#58FF00',
+	25: '#47FF00',
+	26: '#35FF00',
+	27: '#24FF00',
+	28: '#12FF00',
+	29: '#00FF00'
+};
+
+function getProgressBar(percentage, color) {
+	return `<div class="progress"><div class="progress-bar role="progressbar" style="width: ${percentage}%; background-color: ${color}" aria-valuenow="${percentage}" aria-valuemin="0" aria-valuemax="100">${percentage}%</div></div>`;
+}
+
+function processCloneGenProgressPage() {
+	var urlParams = new URLSearchParams(window.location.search);
+	if (!urlParams.has('project')) {
+		alert('Missing project ID');
+	}
+	if (!urlParams.has('run')) {
+		alert('Missing run ID');
+	}
+
+	var projectId = urlParams.get('project');
+	if (!Number.isInteger(parseInt(projectId))) {
+		alert('Unable to get data for Project: ' + projectId);
+	}
+
+	var runId = urlParams.get('run');
+	if (!Number.isInteger(parseInt(runId))) {
+		alert('Unable to get data for Project: ' + projectId + '; Run: ' + runId);
+	}
+
+	$.getJSON("/assets/data/" + projectId + ".json")
+	.done(function(data) {
+		var runData = data.runs.find(run=>run.run==runId);
+		if (!runData) {
+			alert('Unable to get data for Project: ' + projectId + '; Run: ' + runId);
+			return;
+		}
+
+		$('#prcg2Title').html('Progress for Project: ' + projectId + '; Run: ' + runId + '. Last updated at ' + new Date(data.lastUpdated).toLocaleString());
+
+		var dataSeries = [];
+		var dataRows = []
+		var totalGensCompleted = 0;
+		var percentage = 0.0;
+		var colorClassIndex = '';
+		$.each(runData.clones, function(index, clone) {
+			genCount = clone.gen + 1
+			colorClassIndex = Math.max(0, Math.floor((30 * genCount) / data.maxGensPerClone) - 1);
+			percentage = (100 * genCount) / data.maxGensPerClone;
+			dataSeries[index] = { data: [{x: clone.clone,y: 0}, {x: clone.clone, y: clone.gen}], borderColor: colorClass[colorClassIndex], backgroundColor:colorClass[colorClassIndex] };
+			dataRows[index] = { clone: clone.clone, gen: clone.gen, remaining: (data.maxGensPerClone - genCount), progress: getProgressBar(percentage, colorClass[colorClassIndex]) };
+			totalGensCompleted += genCount;
+		});
+		//createCloneGenChart(projectId, runId, data.maxClonesPerRun, data.maxGensPerClone, dataSeries);
+		$('#prcg2Table').bootstrapTable({data: dataRows, formatNoMatches: function () {return 'No data found.';}});
+		var totalGensForRun = data.maxClonesPerRun * data.maxGensPerClone;
+		colorClassIndex = Math.max(0, Math.floor((30 * totalGensCompleted) / totalGensForRun) - 1);
+		percentage = (100 * totalGensCompleted) / totalGensForRun;
+		$('#prcg2ProgressBar').html(getProgressBar(percentage, colorClass[colorClassIndex]));
+	})
+	.fail(function(data) {
+		// The project specified in the URL does not point to a valid project or there isn't data yet
+		alert('Unable to get data for Project: ' + projectId);
+	})
+	.always(function(data) {
+		//alert('always');
+	});
+}
+
 $(document).ready(function () {
 	'use strict';
+	// PRCG Progress 2
+	var page = window.location.pathname.split("/").pop();
+	if (page === 'prcgProgress2') {
+		prcgProgress2();
+	}
+
 	// Toggle page description visibility
 	$('#togglePageDescription').on('click', function (e) {
 		e.preventDefault();
