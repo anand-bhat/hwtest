fa17ccd5cfa78a7f1e682e1220dfe7c77bb9c218
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-06-13 17:54:33 +1000

    Update custom.js

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -192,6 +192,8 @@ function prcgProgress2() {
 
 		$('#prcg2Title').html('Progress for Project: ' + projectId + '; Run: ' + runId + '. Last updated at ' + new Date(data.lastUpdated).toLocaleString());
 
+		var abortedGenAlert = ' <svg class="bi bi-exclamation-triangle-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"><title>Trajectory aborted due to failures</title></path></svg>';
+
 		var dataSeries = [];
 		var dataRows = [];
 		var totalGensCompleted = 0;
@@ -202,7 +204,9 @@ function prcgProgress2() {
 			colorClassIndex = Math.max(0, Math.floor((30 * genCount) / data.maxGensPerClone) - 1);
 			percentage =  Math.round((((100 * genCount) / data.maxGensPerClone) + Number.EPSILON) * 100) / 100;
 			dataSeries[index] = { data: [{x: clone.clone, y: 0}, {x: clone.clone, y: Math.max(0, clone.gen)}], borderColor: colorClass[colorClassIndex], backgroundColor:colorClass[colorClassIndex] };
-			dataRows[index] = { clone: clone.clone, gen: clone.gen === -1 ? '-':clone.gen, remaining: (data.maxGensPerClone - genCount), progress: getProgressBar(percentage, colorClass[colorClassIndex]) };
+			var lastCompleted = clone.gen === -1 ? '-' : clone.gen;
+			lastCompleted = clone.aborted ? lastCompleted + abortedGenAlert : lastCompleted;
+			dataRows[index] = { clone: clone.clone, gen: lastCompleted, remaining: (data.maxGensPerClone - genCount), progress: getProgressBar(percentage, colorClass[colorClassIndex]) };
 			totalGensCompleted += genCount;
 		});
 		prcg2Chart(projectId, runId, data.maxClonesPerRun, data.maxGensPerClone, dataSeries);
