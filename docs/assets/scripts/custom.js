0db738b414a5f41fb6220a2060c079c766b32f14
Anand Bhat <anand-bhat@users.noreply.github.com>
2020-06-28 20:06:18 +1000

    Update custom.js

--- docs/assets/scripts/custom.js
+++ docs/assets/scripts/custom.js
@@ -114,7 +114,7 @@ function failedAlert(failedCount, project, run, clone, gen) {
 	var entity = failedCount > 1 ? 'trajectories' : 'trajectory';
 	var alertImage = `<svg class="bi bi-exclamation-triangle-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"><title>${failedCount} ${entity} aborted</title></path></svg>`;
 	if (project != null && run != null && clone != null && gen != null) {
-		alertImage = `<a href="https://apps.foldingathome.org/wu#project=${project}&run=${run}&clone=${clone}&gen=${gen}" rel="noopener" target="_blank">${alertImage}</a>`
+		alertImage = `<a href="https://apps.foldingathome.org/wu#project=${project}&run=${run}&clone=${clone}&gen=${gen}" rel="noopener" target="_blank">${alertImage}</a>`;
 	}
 	return ` ${alertImage}`;
 }
@@ -342,7 +342,7 @@ function prcgProgress2() {
 			metricsClone[index] = { clone: clone.clone, gen: lastCompleted, trajLength: round(totalGensSuccessfulForClone * data.trajLengthPerWU, 3), completed: totalGensSuccessfulForClone, failed: totalGensFailedForClone, aborted: totalGensAbortedForClone, remaining: totalGensRemainingForClone, progressVal: percentage, progress: getProgressBar(percentage, colorClass[colorClassIndex]) };
 		});
 
-		var metricsRun = []
+		var metricsRun = [];
 		// Run level metrics
 		metricsRun[0] = { wuPlanned: totalGensForRun, wuCompleted: totalGensSuccessfulForRun, wuFailed: totalGensFailedForRun, wuAborted: totalGensAbortedForRun, wuRemaining: totalGensRemainingForRun, trajPlanned: round(totalGensForRun * data.trajLengthPerWU, 3), trajCompleted: round(totalGensSuccessfulForRun * data.trajLengthPerWU, 3), trajFailed: round(totalGensFailedForRun * data.trajLengthPerWU, 3), trajAborted: round(totalGensAbortedForRun * data.trajLengthPerWU, 3), trajRemaining: round(totalGensRemainingForRun * data.trajLengthPerWU, 3) };
 
