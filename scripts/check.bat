391cc2020f94bb6bff9133ec8e076797a0414a21
Anand Bhat <anandbhat2509@gmail.com>
2020-08-06 16:47:04 +1000

    T

--- scripts/check.bat
+++ scripts/check.bat
@@ -1,4 +1,9 @@
+python projectProgressChecker.py -f ../docs/assets/data/11765.json
+python projectProgressChecker.py -f ../docs/assets/data/11766.json
 python projectProgressChecker.py -f ../docs/assets/data/11767.json
+python projectProgressChecker.py -f ../docs/assets/data/11768.json
+python projectProgressChecker.py -f ../docs/assets/data/11769.json
+python projectProgressChecker.py -f ../docs/assets/data/11770.json
 python projectProgressChecker.py -f ../docs/assets/data/14184.json
 python projectProgressChecker.py -f ../docs/assets/data/14290.json
 python projectProgressChecker.py -f ../docs/assets/data/14291.json
@@ -11,9 +16,12 @@ python projectProgressChecker.py -f ../docs/assets/data/14297.json
 python projectProgressChecker.py -f ../docs/assets/data/14298.json
 python projectProgressChecker.py -f ../docs/assets/data/14299.json
 python projectProgressChecker.py -f ../docs/assets/data/14520.json
+python projectProgressChecker.py -f ../docs/assets/data/14521.json
 python projectProgressChecker.py -f ../docs/assets/data/14524.json
 python projectProgressChecker.py -f ../docs/assets/data/14836.json
 python projectProgressChecker.py -f ../docs/assets/data/16800.json
 python projectProgressChecker.py -f ../docs/assets/data/16801.json
 python projectProgressChecker.py -f ../docs/assets/data/16804.json
+python projectProgressChecker.py -f ../docs/assets/data/16920.json
+
 cmd
