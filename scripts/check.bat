670c40f6dbce576bbe2f1a9058b413399f6aa6fe
Anand Bhat <anandbhat2509@gmail.com>
2020-08-26 15:06:43 +1000

    T

--- scripts/check.bat
+++ scripts/check.bat
@@ -3,6 +3,8 @@ python projectProgressChecker.py -f ../docs/assets/data/14526.json
 python projectProgressChecker.py -f ../docs/assets/data/14527.json
 python projectProgressChecker.py -f ../docs/assets/data/14528.json
 python projectProgressChecker.py -f ../docs/assets/data/14529.json
+python projectProgressChecker.py -f ../docs/assets/data/14821.json
+python projectProgressChecker.py -f ../docs/assets/data/14903.json
 python projectProgressChecker.py -f ../docs/assets/data/16400.json
 python projectProgressChecker.py -f ../docs/assets/data/16402.json
 python projectProgressChecker.py -f ../docs/assets/data/16416.json
@@ -10,6 +12,7 @@ python projectProgressChecker.py -f ../docs/assets/data/16418.json
 python projectProgressChecker.py -f ../docs/assets/data/16421.json
 python projectProgressChecker.py -f ../docs/assets/data/16448.json
 python projectProgressChecker.py -f ../docs/assets/data/16449.json
+python projectProgressChecker.py -f ../docs/assets/data/16451.json
 python projectProgressChecker.py -f ../docs/assets/data/16600.json
 python projectProgressChecker.py -f ../docs/assets/data/16810.json
 python projectProgressChecker.py -f ../docs/assets/data/16920X.json
