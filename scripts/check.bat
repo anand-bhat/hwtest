2e4b264daff020c6ba2ec6e728c8fbd358527b6c
Anand Bhat <anandbhat2509@gmail.com>
2020-11-25 18:33:19 +1100

    Update check.bat

--- scripts/check.bat
+++ scripts/check.bat
@@ -1,5 +1,5 @@
 python projectProgressChecker.py -f ../docs/assets/data/13831.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14903.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16451.json -st alternate
-python projectProgressChecker.py -f ../docs/assets/data/16456.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/16456.json -st alternate1
 cmd
