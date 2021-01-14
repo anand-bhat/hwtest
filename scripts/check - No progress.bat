b47120a475e7f0b8d4a33c4dffd9d6045c48bf57
Anand Bhat <anandbhat2509@gmail.com>
2021-01-14 22:52:38 +1100

    T

--- scripts/check - No progress.bat
+++ scripts/check - No progress.bat
@@ -11,6 +11,7 @@ python projectProgressChecker.py -f ../docs/assets/data/14296.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14297.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14298.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14299.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/14377.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14500.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14501.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/14502.json -st alternate
@@ -56,6 +57,7 @@ python projectProgressChecker.py -f ../docs/assets/data/16431.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16448.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16449.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16450.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/16451.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16452.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16453.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16454.json -st alternate
--- scripts/check.bat
+++ scripts/check.bat
@@ -3,8 +3,6 @@ python projectProgressChecker.py -f ../docs/assets/data/13828.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/13829.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/13830.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/13831.json -st alternate
-python projectProgressChecker.py -f ../docs/assets/data/14377.json -st alternate
-python projectProgressChecker.py -f ../docs/assets/data/16451.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16812.json -st alternate
 python projectProgressTracker.py
 cmd
