e3f0279b62adf39e47faa6991d9d8ec424b843e5
Anand Bhat <anandbhat2509@gmail.com>
2021-01-17 01:52:14 +1100

    T

--- scripts/check.bat
+++ scripts/check.bat
@@ -1,5 +1,15 @@
+python projectProgressChecker.py -f ../docs/assets/data/13436.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/13831.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16812.json -st alternate
 python projectProgressChecker.py -f ../docs/assets/data/16814.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17201.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17202.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17206.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17207.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17208.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17209.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17210.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17211.json -st alternate
+python projectProgressChecker.py -f ../docs/assets/data/17212.json -st alternate
 python projectProgressTracker.py
 cmd
